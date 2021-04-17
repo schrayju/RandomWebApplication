3d378181814cde66cec84d0c90c3dc352fb9d11c
mto99 <ozmen.muhammed@outlook.com>
2021-04-17 11:06:22 +0200

    Verzeichnisstruktur der Website geändert. Dateien nach Datentyp in Ordner eingeteilt. (Branch Shop gelöscht).

---
+++ Shopseiten/Javascript/anmeldung.js
@@ -0,0 +1,29 @@
+//check if both passwords are equal
+function comparePasswords(){
+    var pw1 = document.getElementById("passwort").value;
+    var pw2 = document.getElementById("passwort_wdh").value;
+
+    if (pw1 != pw2){
+        document.getElementById("alertPassword").innerHTML = "Passwörter stimmen nicht überein!";
+        return false;
+    }
+
+    else{
+        document.getElementById("alertPassword").innerHTML = "";
+        return true;
+    }
+}
+
+
+//check if username is already used
+function checkUsername(){
+    //search for username in database
+}
+
+
+//submit new users data
+function submitData(){
+    alert("Erfolgreich registiert!")
+    //create new entry for user in database
+}
+
---
+++ Shopseiten/Javascript/kontakt.js
@@ -0,0 +1,5 @@
+//send form
+function sendFrom(){
+    alert("Formular gesendet!")
+    //send form to email
+}
