1dc83372a54a7188e5eaaf01f8ac9d8a7764067c
mto99 <ozmen.muhammed@outlook.com>
2021-05-12 09:04:17 +0200

    Frontend

---
+++ Website/Frontend/Javascript/sessionHandling.js
@@ -0,0 +1,85 @@
+//Eigener Code
+function selectPath(param){
+    localStorage.setItem('Oberkategorie', param);
+}
+
+function selectCat(param, id){
+    localStorage.setItem('Unterkategorie', param);
+    localStorage.setItem('UnterkategorieID', id);
+}
+
+function selectID(param){
+    localStorage.setItem('ProduktID',param);
+}
+
+//--------------------------------------------
+
+// sets or overwrites an value in local storage
+function setSessionItem(label, value) {
+    localStorage.setItem(label, value);
+}
+
+// retreives an value from localStorage by label
+// returns null if not existent
+function getSessionItem(label) {
+    return localStorage.getItem(label);
+}
+
+// checks, if an item exists in local storage
+function existsSessionItem(label) {
+    return !isNullOrUndefined(getSessionItem(label));
+}
+
+// sets or overwrites an json object as value to local storage
+function setJSONSessionItem(label, jsonValue) {
+    setSessionItem(label, JSON.stringify(jsonValue));
+}
+
+// retreives an json object from local storage
+// if not existent returns null
+// if json string converts to json object
+function getJSONSessionItem(label) {
+    var val = getSessionItem(label);
+
+    // if undefined (not existent), return undefined
+    if (isNullOrUndefined(val)) 
+        return val;
+
+    // if json string, convert and return as json object
+    if (isJSONString(val)) 
+        return tryParseJSONString(val);
+
+    // otherwise return as string
+    return val;
+}
+
+// removes a session item by label
+function removeSessionItem(label) {
+    localStorage.removeItem(label);
+}
+
+// clears complete session / deletes all session items
+function clearSession() {
+    localStorage.clear();
+}
+
+// try parse JSON string
+// returns false if no json string otherwise the JSON object
+function tryParseJSONString(str) {
+    try {
+        var obj = JSON.parse(str);
+        if (obj && typeof obj === "object") 
+            return obj;
+    } catch (e) { }
+    return false;
+}
+
+// check if given string is a json string
+function isJSONString(str) {
+    return tryParseJSONString(str) != false;
+}
+
+// function checks if given value is null or undefined
+function isNullOrUndefined(val) {
+    return val === null || val === undefined;
+}
