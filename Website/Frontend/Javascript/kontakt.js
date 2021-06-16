85e60935c7f61635e15380d6c42faa582f61167e
schrayju <48958077+schrayju@users.noreply.github.com>
2021-06-16 13:03:21 +0200

    Update kontakt.js

--- Website/Frontend/Javascript/kontakt.js
+++ Website/Frontend/Javascript/kontakt.js
@@ -1,5 +1,31 @@
 //send form
-function sendFrom(){
-    alert("Formular gesendet!")
-    //send form to email
+function sendFrom(betreff, textarea){
+
+    // Objekt Erzeugung
+    var nodemailer = require('nodemailer');
+    // Methode um den Gmail Account zu nutzen
+    var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'kontakt.randomwebapplication@gmail.com',
+        pass: 'Albstadt007'
+      }
+    });
+    
+    var mailOptions = {
+      from: 'kontakt.randomwebapplication@gmail.com',
+      to: 'kontakt.randomwebapplication@gmail.com',
+      subject: betreff,
+      text: textarea
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+        alert("Ihr Schreiben wurde erfolgreich an uns weitergeleitet!");
+      }
+    });
+    
 }
